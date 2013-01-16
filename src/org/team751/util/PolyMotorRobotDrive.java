ba9f81806656e25ba5c7c5b5a19e21b6ca6b88b5
samcrow <>
2013-01-15 17:46:42 -0800

    PMD now preliminarily done. It should be tested.

--- src/org/team751/util/PolyMotorRobotDrive.java
+++ src/org/team751/util/PolyMotorRobotDrive.java
@@ -1,7 +1,6 @@
 package org.team751.util;
 
 import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 /**
@@ -23,12 +22,14 @@ public class PolyMotorRobotDrive {
      * The CAN sync group for the left motors Jaguars set with this sync group
      * will update their output at the same time when
      * {@link edu.wpi.first.wpilibj.CANJaguar#updateSyncGroup(byte)} is called.
+     * Other Jaguars on the network must not use this sync group.
      */
     protected static final byte LEFT_SYNCGROUP = 1;
     /**
      * The CAN sync group for the right motors Jaguars set with this sync group
      * will update their output at the same time when
      * {@link edu.wpi.first.wpilibj.CANJaguar#updateSyncGroup(byte)} is called.
+     * Other Jaguars on the network must not use this sync group.
      */
     protected static final byte RIGHT_SYNCGROUP = 2;
 
@@ -42,6 +43,48 @@ public class PolyMotorRobotDrive {
         this.leftMotors = leftMotors;
         this.rightMotors = rightMotors;
     }
+    
+    /**
+     * Drive the robot with arcade drive
+     * @param moveValue The degree to which the robot should be moved
+     * forward/back. Full forward is +1, full reverse is -1
+     * @param rotateValue The degree to which the robot should turn left
+     * or right. Full left is -1, full right is +1.
+     */
+    public void arcadeDrive(double moveValue, double rotateValue) {
+        //Based on http://www.chiefdelphi.com/media/papers/2661?langid=2
+        
+        double max = Math.abs(moveValue);
+        if(Math.abs(rotateValue) > max) {
+            max = Math.abs(rotateValue);
+        }
+        double sum = moveValue + rotateValue;
+        double difference = moveValue - rotateValue;
+        
+        double leftPower;
+        double rightPower;
+        
+        if(moveValue > 0) {
+            if(rotateValue >= 0) {
+                leftPower = max;
+                rightPower = difference;
+            } else {
+                leftPower = sum;
+                rightPower = max;
+            }
+        } else {
+            if(rotateValue >= 0) {
+                leftPower = sum;
+                rightPower = -max;
+            } else {
+                leftPower = -max;
+                rightPower = difference;
+            }
+        }
+        
+        setLeftRightMotorOutputs(leftPower, rightPower);
+    }
+    
 
     protected void setLeftRightMotorOutputs(double leftOutput, double rightOutput) {
         //Set the output of each left motor (they won't actually update
@@ -70,4 +113,6 @@ public class PolyMotorRobotDrive {
             ex.printStackTrace();
         }
     }
+    
+    
 }
