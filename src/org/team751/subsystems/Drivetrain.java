6513ebc064026d701c2552efa99bd7f1967d2f2b
samcrow <sp4.cc.eatpie.info@gmail.com>
2013-02-01 16:03:50 -0800

    Cleaned up the fallout from cherrypicking a recent commit to branch summer-robot that fixed drivetrain timing issues.

--- src/org/team751/subsystems/Drivetrain.java
+++ src/org/team751/subsystems/Drivetrain.java
@@ -1,13 +1,11 @@
 package org.team751.subsystems;
 
 import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.team751.resources.CANJaguarIDs;
 import org.team751.commands.JoystickDrive;
-import org.team751.resources.DigitalChannels;
+import org.team751.resources.CANJaguarIDs;
 import org.team751.util.PolyMotorRobotDrive;
 
 /**
@@ -19,17 +17,18 @@ public class Drivetrain extends Subsystem {
     // here. Call these from Commands.
     
     //Left side Jaguars
-    SpeedController left1;
-    SpeedController left2;
-    SpeedController left3;
+    private SpeedController left1;
+    private SpeedController left2;
+    private SpeedController left3;
     //Right side Jaguars
-    SpeedController right1;
-    SpeedController right2;
-    SpeedController right3;
+    private SpeedController right1;
+    private SpeedController right2;
+    private SpeedController right3;
+    
+    private PolyMotorRobotDrive drive;
     
     
-    
-    PolyMotorRobotDrive drive;
+    private long lastRunTime = System.currentTimeMillis();
 
     public Drivetrain() {
         try {
@@ -46,9 +45,37 @@ public class Drivetrain extends Subsystem {
         }
         drive = new PolyMotorRobotDrive(
                 new SpeedController[]{left1, left2, left3}, new SpeedController[]{right1, right2, right3});
+    
+        //Timing note: The software sometimes does other things for a pretty
+        //long time (100-500 milliseconds) between running the drive command.
+        //When this happens, the motor safety mechanism stops the motors.
+        //This is not good.
+        //This sets the motor safety mechanism to not stop the motors
+        //until after 1 second of non-response.
+
+        try {
+            ((CANJaguar) left1).setExpiration(1);
+            ((CANJaguar) left2).setExpiration(1);
+            ((CANJaguar) left3).setExpiration(1);
+            ((CANJaguar) right1).setExpiration(1);
+            ((CANJaguar) right2).setExpiration(1);
+            ((CANJaguar) right3).setExpiration(1);
+        } catch (ClassCastException e) {
+        }
+    
     }
     
     public void arcadeDrive(double moveValue, double rotateValue) {
+        //timing
+        long now = System.currentTimeMillis();
+        long loopTime = now - lastRunTime;
+
+        lastRunTime = now;
+
+        if (loopTime > 100) {
+            System.out.println("Long loop! " + loopTime + " ms");
+        }
+        
         drive.arcadeDrive(moveValue, rotateValue);
     }
     
@@ -58,3 +85,4 @@ public class Drivetrain extends Subsystem {
         setDefaultCommand(new JoystickDrive());
     }
 }
+
