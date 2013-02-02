3891e52368b6a7d714f32f96ead657125c1aa4b6
samcrow <sp4.c.eatpie.info@gmail.com>
2013-02-02 07:23:04 -0800

    PeriodicTask finished

--- src/org/team751/util/PeriodicTask.java
+++ src/org/team751/util/PeriodicTask.java
@@ -1,6 +1,8 @@
 package org.team751.util;
 
+import com.sun.squawk.util.MathUtils;
 import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A base class for anything that should run periodically, independent of
@@ -10,18 +12,52 @@ import java.util.Timer;
  */
 public abstract class PeriodicTask {
     
-    private Timer timer = new Timer();
+    private Timer periodicTaskTimer = new Timer();
     
     /**
      * The time, in seconds, that should pass between times when the task
-     * is run.
+     * is run. Subclasses can override this with {@link #setTaskTime(double) }.
      */
     private double periodicTaskTime = 1;
     
     /**
-     * The run method
+     * Set the time, in seconds, to wait in between calls to
+     * {@link #run() }. This should be called before {@link #start()}. Otherwise,
+     * it will have no effect.
+     * @param newTime The time in seconds in between processing loops
+     */
+    protected void setTaskTime(double newTime) {
+        periodicTaskTime = newTime;
+    }
+    
+    public void start() {
+        periodicTaskTimer.scheduleAtFixedRate(periodicTask, 0, MathUtils.round(periodicTaskTime * 1000));
+    }
+    
+    /**
+     * The run method.
      * The timer will call this method.
      * Subclasses should place code in this method to be run.
      */
     protected abstract void run();
+    
+    
+    /**
+     * The task that calls {@link #run()}
+     */
+    private TimerTask periodicTask = new TimerTask() {
+
+        public void run() {
+            /*
+             * Advanced Java: This is a method inside an inner class.
+             * Because this class is not static, it has access to the fields
+             * and methods of the instances of the outer PeriodicTask as well as
+             * the inner TimerTask. We can use the <class name>.this.<method clall>
+             * syntax to specify that we want to access the outer instance.
+             */
+            //call the run() method of the outer PeriodicTask instance
+            PeriodicTask.this.run();
+        }
+        
+    };
 }
