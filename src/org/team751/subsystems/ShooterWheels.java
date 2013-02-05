2b95f481d91c7eca1bc3df6766de3a8f0b4539a1
samcrow <sp4.c.eatpie.info@gmail.com>
2013-02-04 19:45:13 -0800

    More subsystem implementation work

--- src/org/team751/subsystems/ShooterWheels.java
+++ src/org/team751/subsystems/ShooterWheels.java
@@ -1,6 +1,9 @@
 package org.team751.subsystems;
 
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.team751.resources.CANJaguarIDs;
 
 /**
  * A Subsystem that includes the shooter wheels and sensors for measuring their
@@ -8,9 +11,47 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @author Sam Crow
  */
 public class ShooterWheels extends Subsystem {
+    
+    /**
+     * The Jaguar controlling the slower wheel that the disk contacts first
+     */
+    private CANJaguar firstMotor;
+    
+    /**
+     * The Jaguar controlling the faster wheel that the disk contacts second
+     */
+    private CANJaguar secondMotor;
+    
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    public ShooterWheels() {
+        try {
+            firstMotor = new CANJaguar(CANJaguarIDs.SHOOTER_FIRST);
+            secondMotor = new CANJaguar(CANJaguarIDs.SHOOTER_SECOND);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    /**
+     * Configure the Jaguars to use PID. This should be called after any Jaguar
+     * restarts.
+     * @throws CANTimeoutException 
+     */
+    private void configJaguars() throws CANTimeoutException {
+        
+        firstMotor.changeControlMode(CANJaguar.ControlMode.kSpeed);
+        secondMotor.changeControlMode(CANJaguar.ControlMode.kSpeed);
+        
+        //Set motors to coast
+        firstMotor.configNeutralMode(CANJaguar.NeutralMode.kCoast);
+        secondMotor.configNeutralMode(CANJaguar.NeutralMode.kCoast);
+        
+        
+    }
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
