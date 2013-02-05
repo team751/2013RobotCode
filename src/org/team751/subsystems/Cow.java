7087c1e60daa45495185fb308d12afc3410f21c8
samcrow <sp4.c.eatpie.info@gmail.com>
2013-02-04 18:57:26 -0800

    Added empty Cow, Feeder, ShooterWheels, and ShooterAngle subsystem classes

---
+++ src/org/team751/subsystems/Cow.java
@@ -0,0 +1,17 @@
+package org.team751.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * A Subsystem that controls the Cow, and its position and angle status.
+ * @author Sam Crow
+ */
+public class Cow extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
---
+++ src/org/team751/subsystems/ShooterAngle.java
@@ -0,0 +1,17 @@
+package org.team751.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * A Subsystem that keeps track of the angle of the shooter and adjusts it
+ * @author Sam Crow
+ */
+public class ShooterAngle extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
