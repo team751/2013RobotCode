9dc28ea9cdd66b38a0cf7726edc9b675077c3ad7
samcrow <sp4.cc.eatpie.info@gmail.com>
2013-02-06 18:01:11 -0800

    Replaced Cow with Cow2

---
+++ src/org/team751/subsystems/Cow2.java
@@ -0,0 +1,14 @@
+package org.team751.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Manages the second, simpler, revision of the cow
+ * @author Sam Crow
+ */
+public class Cow2 extends Subsystem {
+
+    protected void initDefaultCommand() {
+    }
+    
+}
